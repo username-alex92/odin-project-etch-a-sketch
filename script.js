e54c42143267b7fef9bdaecb57a2b84c9716ee46
username-alex92 <80555709+username-alex92@users.noreply.github.com>
2025-05-14 20:50:23 +0300

    Change divs size, add hover effect
    Set up a “hover effect so that the grid divs change color when mouse passes over them

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ container.classList.toggle("container");
 document.body.appendChild(container);
 for (let i = 0; i < 16; i++) {
     let row = document.createElement("div");
+    row.classList.toggle("row");
     container.appendChild(row);
     for (let j = 0; j < 16; j++) {
         let item = document.createElement("div");
         item.classList.toggle("item");
         row.appendChild(item);
     }
+}
+
+container.addEventListener("mouseover", (event) => {
+    event.target.style.background = randomRGB();
+});
+
+function randColorNum() {
+    return Math.floor(Math.random() * 256);
+}
+
+function randomRGB() {
+    return `rgb(${randColorNum()},${randColorNum()},${randColorNum()})`;
 }
